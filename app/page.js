03a0fd475029c78959bb7c49dc97fd37fd65655f
coilt <cameraboi@gmail.com>
2024-08-12 02:52:38 +0700

    Add some front enhancements

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import destr from 'destr';
+import destr from 'destr'
 import { useState } from 'react'
 import axios from 'axios'
 import { MagnifyingGlassIcon, UsersIcon } from '@heroicons/react/20/solid'
@@ -10,66 +10,68 @@ export default function Home() {
   const [processedManifest, setProcessedManifest] = useState(null)
 
   const handleManifestUpload = (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-  
+    const file = event.target.files[0]
+    const reader = new FileReader()
+
     reader.onload = (e) => {
-      const manifestContent = e.target.result;
-  
+      const manifestContent = e.target.result
+
       // Extract packageID using the provided regular expression
       const extractPackageID = (content) => {
-        const regex = /"packageID"\s*:\s*"?(\d+)"?/;
-        const match = content.match(regex);
-        return match ? match[1] : null;
-      };
-  
-      const packageID = extractPackageID(manifestContent);
-      console.log('Extracted packageID:', packageID);
-  
+        const regex = /"packageID"\s*:\s*"?(\d+)"?/
+        const match = content.match(regex)
+        return match ? match[1] : null
+      }
+
+      const packageID = extractPackageID(manifestContent)
+      console.log('Extracted packageID:', packageID)
+
       // Extract the manifest object using regular expressions
       const extractManifestObject = (content) => {
-        const filesRegex = /"Files"\s*:\s*(\[[\s\S]*?\])/;
-        const packageIDRegex = /"packageID"\s*:\s*"?(\d+)"?/;
-        const chunkIDToDependenciesRegex = /"ChunkIDToDependencies"\s*:\s*(\{[\s\S]*?\})/;
-      
-        const filesMatch = content.match(filesRegex);
-        const packageIDMatch = content.match(packageIDRegex);
-        const chunkIDToDependenciesMatch = content.match(chunkIDToDependenciesRegex);
-      
-        const files = filesMatch ? destr(filesMatch[1]) : [];
-        const packageID = packageIDMatch ? packageIDMatch[1] : null;
-        const chunkIDToDependencies = chunkIDToDependenciesMatch ? destr(chunkIDToDependenciesMatch[1]) : {};
-      
+        const filesRegex = /"Files"\s*:\s*(\[[\s\S]*?\])/
+        const packageIDRegex = /"packageID"\s*:\s*"?(\d+)"?/
+        const chunkIDToDependenciesRegex =
+          /"ChunkIDToDependencies"\s*:\s*(\{[\s\S]*?\})/
+
+        const filesMatch = content.match(filesRegex)
+        const packageIDMatch = content.match(packageIDRegex)
+        const chunkIDToDependenciesMatch = content.match(
+          chunkIDToDependenciesRegex
+        )
+
+        const files = filesMatch ? destr(filesMatch[1]) : []
+        const packageID = packageIDMatch ? packageIDMatch[1] : null
+        const chunkIDToDependencies = chunkIDToDependenciesMatch
+          ? destr(chunkIDToDependenciesMatch[1])
+          : {}
+
         return {
           Files: files,
           Dependencies: {
             packageID: packageID,
             ChunkIDToDependencies: chunkIDToDependencies,
           },
-        };
-      };
-      
-      
-  
-      const extractedManifest = extractManifestObject(manifestContent);
-      setManifest(extractedManifest);
-    };
-  
-    reader.readAsText(file);
-  };
-  
+        }
+      }
+
+      const extractedManifest = extractManifestObject(manifestContent)
+      setManifest(extractedManifest)
+    }
+
+    reader.readAsText(file)
+  }
 
   const handleSearch = async () => {
-    console.log('Sending request with manifest:', manifest);
-    console.log('Sending request with searchTerm:', searchTerm);
-    const response = await axios.post('/api/manifest', { manifest, searchTerm });
-    console.log('Response from server:', response.data);
-    setProcessedManifest(response.data);
-  };
+    console.log('Sending request with manifest:', manifest)
+    console.log('Sending request with searchTerm:', searchTerm)
+    const response = await axios.post('/api/manifest', { manifest, searchTerm })
+    console.log('Response from server:', response.data)
+    setProcessedManifest(response.data)
+  }
 
   return (
     <main>
-      <div className='py-80 w-full'>
+      <div className='py-40 w-full'>
         <div>
           <label
             htmlFor='email'
@@ -79,16 +81,12 @@ export default function Home() {
           </label>
           <div className='mt-2 flex  shadow-sm'>
             <div className='relative flex flex-grow items-stretch focus-within:z-10'>
-              
-            
               <input
                 type='file'
-              
                 accept='.json'
                 onChange={handleManifestUpload}
-                className='w-24 relative -ml-px inline-flex items-center gap-x-1.5 rounded-l-md px-3 py-2 text-sm font-semibold text-slate-400 ring-1 ring-inset ring-slate-800 hover:bg-slate-900'
+                className='w-24 relative -ml-px -mr-px inline-flex items-center gap-x-1.5 rounded-l-md px-3 py-2 text-sm font-semibold text-slate-400 ring-1 ring-inset ring-slate-800 hover:bg-slate-900'
               />
-
             </div>
             <input
               id='search'
@@ -97,7 +95,7 @@ export default function Home() {
               aria-describedby='email-optional'
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
-              className='block w-full border-0 py-1.5 pl-8 text-slate-900 ring-1 ring-inset ring-indigo-600 placeholder:text-slate-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+              className='block w-full border-0 py-1.5 pl-8 text-slate-400 ring-1 ring-inset ring-slate-800 placeholder:text-slate-600 focus:ring-2 focus:ring-inset focus:ring-indigo-900 sm:text-sm sm:leading-6'
             />
             <button
               type='button'
@@ -112,9 +110,13 @@ export default function Home() {
             </button>
           </div>
         </div>
+        <div className='spacer'></div>
+        <div className='output bg-gray-950 p-8 rounded-xl' >
+
         {processedManifest && (
           <pre>{JSON.stringify(processedManifest, null, 2)}</pre>
         )}
+        </div>
       </div>
     </main>
   )
